856f4edc6e5033292112309af1bd726c2956f881
distinctharsh <harshsingh55818@gmail.com>
2025-09-08 00:10:57 +0530

    offer part

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import HeroSlider from "../components/HeroSlider";
 import AnnouncementBar from "../components/AnnouncementBar";
 import PmQuote from "../components/PmQuote";
 import AboutSection from "@/components/AboutSection";
+import Offerings from "../components/Offerings";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <AnnouncementBar />
       <PmQuote />
       <AboutSection />
+  <Offerings />
       <main id="main" className="p-6">
         
       </main>
