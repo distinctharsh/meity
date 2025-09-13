d87e879841ef8a87753d5a570468bae905c299d7
distinctharsh <harshsingh55818@gmail.com>
2025-09-13 21:11:14 +0530

    updates

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <main id="main" className="p-6">
+      <main id="main" >
       <HeroSlider />
       <AnnouncementBar />
       <PmQuote />
