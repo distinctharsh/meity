51d92b1e74de26e03d18c1c249edc3f63bab1ab0
distinctharsh <harshsingh55818@gmail.com>
2025-09-13 22:36:14 +0530

    updates

--- components/RecentDocs.js
+++ components/RecentDocs.js
@@ -42,7 +42,7 @@ const RecentDocs = () => {
       <div className="flex flex-row justify-between items-start w-full max-w-[1640px] px-4">
 
         {/* Recent Documents */}
-        <div className="flex flex-col items-start flex-2 mr-[30px]">
+        <div className="flex flex-col items-start flex-[2] mr-[30px]">
           <div className="flex items-center mb-3">
             <Document className="w-5 h-5 mr-2" />
             <h3 className="text-[#202A3A] text-[1.4rem] font-bold leading-none tracking-tight m-0">Recent Documents</h3>
@@ -68,7 +68,7 @@ const RecentDocs = () => {
         </div>
 
         {/* User Personas */}
-        <div className="flex flex-col items-center justify-center flex-1">
+        <div className="flex flex-col items-center justify-center flex-[1]">
           <div className="flex items-center mb-3">
             <UserPersonas className="w-5 h-5 mr-2" />
             <h3 className="text-[#202A3A] text-[1.4rem] font-bold leading-none tracking-tight m-0">Explore User Personas</h3>
@@ -105,7 +105,7 @@ const RecentDocs = () => {
         </div>
 
         {/* Important Links */}
-        <div className="flex flex-col items-start flex-1 ml-[30px]">
+        <div className="flex flex-col items-start flex-[1] ml-[30px]">
           <div className="flex items-center mb-3">
             <ImportantLink className="w-5 h-5 mr-2" />
             <h3 className="text-[#202A3A] text-[1.4rem] font-bold leading-none tracking-tight m-0">Important Links</h3>
